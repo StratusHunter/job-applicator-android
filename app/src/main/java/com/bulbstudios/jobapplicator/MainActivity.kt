9b5fc8a66135e2b2671c39f4a20d82b9f2d12682
Terence Baker <terence@bulbstudios.com>
2019-03-04 11:21:24 +0000

    initial commit. folder setup. dependency adding. general housekeeping

---
+++ app/src/main/java/com/bulbstudios/jobapplicator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bulbstudios.jobapplicator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
