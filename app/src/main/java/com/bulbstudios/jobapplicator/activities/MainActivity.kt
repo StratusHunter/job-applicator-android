c26a186fd6d220f27461c529d01ac101cbabcb34
Terence Baker <terence@bulbstudios.com>
2019-03-04 11:26:49 +0000

    added strings

--- app/src/main/java/com/bulbstudios/jobapplicator/activities/MainActivity.kt
+++ app/src/main/java/com/bulbstudios/jobapplicator/activities/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.bulbstudios.jobapplicator
+package com.bulbstudios.jobapplicator.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bulbstudios.jobapplicator.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
